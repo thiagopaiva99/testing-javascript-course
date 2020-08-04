cb52e39110ffe9b20cce57591136bab6c1cee17f
thiagopaiva99 <thiagopaiva99@gmail.com>
2020-08-03 22:03:45 -0300

    Disable Unnecessary ESLint Stylistic Rules with eslint-config-prettier

--- static-analysis-testing-javascript-application/src/example.js
+++ static-analysis-testing-javascript-application/src/example.js
@@ -1,10 +1,9 @@
-const username = "freddy";
-typeof username === "string";
+const username = 'freddy'
+typeof username === 'string'
 
-if (!("serviceWorker" in navigator)) {
+if (!('serviceWorker' in navigator)) {
   // you have an old browser :-(
 }
 
-const greeting = "hello";
-console.log(`${greeting} world!`);
-[1, 2, 3].forEach((x) => console.log(x));
+const greeting = 'hello'
+console.log(`${greeting} world!`)[(1, 2, 3)].forEach(x => console.log(x))
