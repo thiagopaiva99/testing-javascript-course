70cf9b7381d7051e72faed298819fc7605754305
thiagopaiva99 <thiagopaiva99@gmail.com>
2020-08-08 23:00:34 -0300

    Make a shared JavaScript mock module

---
+++ javascript-mocking-fundamentals/src/__no-framework-mocks__/utils.js
@@ -0,0 +1,13 @@
+function fn(impl = () => {}) {
+    const mockFn = (...args) => {
+      mockFn.mock.calls.push(args)
+      return impl(...args)
+    }
+    mockFn.mock = {calls: []}
+    return mockFn
+  }
+  
+  module.exports = {
+    getWinner: fn((p1, p2) => p1)
+  }
+  
